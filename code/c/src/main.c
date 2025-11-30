4d6d0c8f531d46b5dd5e3ca295bb8a0dc12a91f4
Joan Frago <jfragojimenez@gmail.com>
2025-11-30 21:42:42 +0100

    remove tcp_server stuff from main file

--- code/c/src/main.c
+++ code/c/src/main.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <unistd.h>
-#include <string.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <signal.h>
@@ -50,93 +46,6 @@ void exit_handler(){
 	exit(0);
 }
 
-void *start_tcp_server(void* arg){
-	printf("Server Thread ID is %lu\n",(unsigned long)pthread_self());
-
-	int sockfd, new_sockfd;
-	struct sockaddr_in server_addr;
-	struct sockaddr_in remote_addr;
-	socklen_t addrlen;
-
-	// Socket creation
-	sockfd = socket(PF_INET, SOCK_STREAM, 0);
-	if(sockfd < 0){
-		perror("Error creating the socket\n");
-		return NULL;
-	}
-
-	// Bind the socket
-	server_addr.sin_family = PF_INET;
-	server_addr.sin_port = htons(8080);
-	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-	int ret = bind(sockfd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr));
-	if(ret < 0){
-		perror("Error creating the socket\n");
-		return NULL;
-	}
-
-	// Listen for connections
-	ret = listen(sockfd,5);
-	if(ret < 0){
-		perror("Error listening for connections\n");
-		return NULL;
-	}
-
-	addrlen = sizeof(remote_addr);
-
-	char *address = inet_ntoa(server_addr.sin_addr);
-	int port = ntohs(server_addr.sin_port);
-
-	printf("Server listening on address %s port %d\n",address,port);
-
-	for(;;){
-		new_sockfd = accept(sockfd, (struct sockaddr *)&remote_addr, &addrlen);
-		if(new_sockfd < 0){
-			perror("Error accepting a connection\n");
-			continue;
-		}
-
-		printf("New client connected\n");
-
-		while(1){
-			// Receive a message
-			char recv_buf[MESSAGE_SIZE + 1];
-			ssize_t bytes_recv = recv(new_sockfd, recv_buf, MESSAGE_SIZE, 0);
-			if(bytes_recv == 0){
-				// Client closed connection (EOF)
-				printf("Client closed connection\n");
-				break;
-			}else if(bytes_recv < 0){
-				perror("Error receiving data\n");
-				break;
-			}
-
-
-			// Build a response message
-			recv_buf[bytes_recv] = '\0'; // Null-terminate the received data
-
-			char *base_msg = "Received data: ";
-			size_t resp_len = strlen(base_msg) + bytes_recv + 1;
-			char resp_buf[resp_len];
-
-			strcpy(resp_buf, base_msg);
-			strncat(resp_buf, recv_buf, MESSAGE_SIZE);
-			printf("%s",resp_buf);
-
-			// Send a response
-			send(new_sockfd, resp_buf, strlen(resp_buf), 0);
-		}
-
-		close(new_sockfd);
-		printf("Connection closed and ready for new client\n");
-	}
-
-	// Close the socket
-	close(sockfd);
-	return 0;
-}
-
 /*
  * Set all devices before running.
  * Read from db and init Devices.
@@ -168,6 +77,8 @@ int set_devices(){
 }
 
 /*
+ * Infinite loop handling each Device's properties.
+ * E.g. Historify and Date scheduling.
  * Only Devices must be used.
  * A single Relay or DigitalInput must not be used
  * since it has no logic attached to it.
@@ -177,7 +88,7 @@ void *core(void* arg){
 
 	for(int i = 0; i < MAX_DEVICES; i++){
 		if(devices[i].rl->id_pin != NULL){
-			relay_write(devices[i].rl, 1);
+			relay_write(devices[i].rl, 0);
 		}
 		if(devices[i].di->id_pin != NULL){
 			int di_status = 0;
