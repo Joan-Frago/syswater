581c1f33983d73a2b43041bd3c3123733ad00069
Joan Frago <jfragojimenez@gmail.com>
2025-12-20 21:00:12 +0100

    Move project

--- code/c/inc/config.h
+++
@@ -1,3 +0,0 @@
-extern char *UNIPI_SYS_BASE_DIR;
-
-int load_config(void);
--- code/c/inc/tcp_server.h
+++
@@ -1,5 +0,0 @@
-#define MESSAGE_SIZE 1000
-
-void *start_tcp_server(void*);
-int talk(int *);
-int process_recv(char *buf, int buf_len);
